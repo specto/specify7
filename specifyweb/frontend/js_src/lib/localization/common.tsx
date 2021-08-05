0cff4760ccd241297ebc0db108bc237494c3e85e
Max Patiiuk <max@patii.uk>
2021-08-05 16:47:41 +0300

    Deduplicate localization strings

--- specifyweb/frontend/js_src/lib/localization/common.tsx
+++ specifyweb/frontend/js_src/lib/localization/common.tsx
@@ -57,6 +57,8 @@ const commonText = createDictionary({
   metadata: 'Metadata',
   query: 'Query',
   unmapped: 'Unmapped',
+  expand: 'Expand',
+  geoMap: 'GeoMap',
 
   // Toolbar
   notifications: (count: number) => `Notifications: ${count}`,
@@ -96,8 +98,8 @@ const commonText = createDictionary({
   queriesDialogTitle: (count: number) => `Queries (${count})`,
   newQueryDialogTitle: 'New Query Type',
   exportQueryForDwca: 'Export query for DwCA definition.',
-  exportQueryForDwcaDialogTitle: 'Query XML for DwCA definition.',
-  exportQueryForDwcaDialogHeader: createHeader(''),
+  exportQueryForDwcaDialogTitle: 'Query XML for DwCA definition',
+  exportQueryForDwcaDialogHeader: createHeader('Query XML for DwCA definition'),
   exportQueryAsReport: 'Define report based on query.',
   exportQueryAsLabel: 'Define label based on query.',
   newResourceTitle: (resourceName: string) => `New ${resourceName}`,
@@ -157,6 +159,7 @@ const commonText = createDictionary({
   secondarySearch: 'Secondary Search',
   running: 'Running...',
   noMatches: 'No Matches',
+  searchQuery: 'Search Query',
 
   // Unload Protection
   leavePageDialogTitle: 'Unsaved changes detected',
