2e3f1391119359e3dea422f2e00745849d020732
Max Patiiuk <max@patii.uk>
2021-10-18 18:09:31 -0500

    Add 'en-us' localization strings
    File conversion was done by:
    1. Pasting file content into an HTML <textbox>
    2. Assigning the `textbox` variable to the HTML Textbox element
    3. Running this code in the DevTools console:
       ```js
       textarea.value = textarea.value.replaceAll(/:\s+([\s\S]*?),\n(?:\n|}|  (?!( |])))/g,": {\n    'en-us'$&},\n")
       ```
    4. Copying the contents of the textbox back into the file

--- specifyweb/frontend/js_src/lib/localization/admin.tsx
+++ specifyweb/frontend/js_src/lib/localization/admin.tsx
@@ -3,42 +3,91 @@ import { createDictionary, createHeader } from './utils';
 // Refer to "Guidelines for Programmers" in ./README.md before editing this file
 
 const adminText = createDictionary({
-  removeAdmin: 'Remove Admin',
-  makeAdmin: 'Make Admin',
-  saveUserFirst: 'Save user first',
-  mustBeManager: 'User must be saved as Manager first',
-
-  mimetype: 'Mimetype:',
-  loadFile: 'Load File',
-  download: 'Download',
-  corruptResourceOrConflict: `
-    This app resource appears to be corrupt but may be in the process of
-    being saved by another session. It can be deleted if that is not the case.`,
-  resourceLoadDialogTitle: 'Load file',
-  resourceLoadDialogHeader: createHeader('Load resource file'),
-  resourceLoadDialogMessage: 'Select the file to be loaded into the editor.',
-  globalResourcesTitle: (resourceCount: string) =>
-    `Global <small>(${resourceCount})</small>`,
-  disciplineResourcesTitle: (resourceCount: string) =>
-    `Discipline <small>(${resourceCount})</small>`,
-  userTypes: 'User Types',
-  users: 'Users',
-  createResourceDialogTitle: 'Create Resource',
-  createResourceDialogHeader: createHeader('Create New Resource File'),
-  newResourceName: 'New Resource Name:',
-
-  setPassword: 'Set Password',
-  passwordLengthError: 'Password must have at least six characters.',
-  passwordsDoNotMatchError: 'Passwords do not match.',
-  saveUserBeforeSettingPasswordError: 'Save user before setting password.',
-
+  removeAdmin: {
+    'en-us': 'Remove Admin',
+  },
+  makeAdmin: {
+    'en-us': 'Make Admin',
+  },
+  saveUserFirst: {
+    'en-us': 'Save user first',
+  },
+  mustBeManager: {
+    'en-us': 'User must be saved as Manager first',
+  },
+  mimetype: {
+    'en-us': 'Mimetype:',
+  },
+  loadFile: {
+    'en-us': 'Load File',
+  },
+  download: {
+    'en-us': 'Download',
+  },
+  corruptResourceOrConflict: {
+    'en-us': `
+      This app resource appears to be corrupt but may be in the process of
+      being saved by another session. It can be deleted if that is not the
+      case.`,
+  },
+  resourceLoadDialogTitle: {
+    'en-us': 'Load file',
+  },
+  resourceLoadDialogHeader: {
+    'en-us': createHeader('Load resource file'),
+  },
+  resourceLoadDialogMessage: {
+    'en-us': 'Select the file to be loaded into the editor.',
+  },
+  globalResourcesTitle: {
+    'en-us': (resourceCount: string) =>
+      `Global <small>(${resourceCount})</small>`,
+  },
+  disciplineResourcesTitle: {
+    'en-us': (resourceCount: string) =>
+      `Discipline <small>(${resourceCount})</small>`,
+  },
+  userTypes: {
+    'en-us': 'User Types',
+  },
+  users: {
+    'en-us': 'Users',
+  },
+  createResourceDialogTitle: {
+    'en-us': 'Create Resource',
+  },
+  createResourceDialogHeader: {
+    'en-us': createHeader('Create New Resource File'),
+  },
+  newResourceName: {
+    'en-us': 'New Resource Name:',
+  },
+  setPassword: {
+    'en-us': 'Set Password',
+  },
+  passwordLengthError: {
+    'en-us': 'Password must have at least six characters.',
+  },
+  passwordsDoNotMatchError: {
+    'en-us': 'Passwords do not match.',
+  },
+  saveUserBeforeSettingPasswordError: {
+    'en-us': 'Save user before setting password.',
+  },
   // PasswordChange
-  password: 'Password',
-  confirmPassword: 'Confirm',
-
+  password: {
+    'en-us': 'Password',
+  },
+  confirmPassword: {
+    'en-us': 'Confirm',
+  },
   // UserCollectionsPlugin
-  collections: 'Collections',
-  userCollectionsPluginDialogTitle: 'Select user collection access',
+  collections: {
+    'en-us': 'Collections',
+  },
+  userCollectionsPluginDialogTitle: {
+    'en-us': 'Select user collection access',
+  },
 });
 
 export default adminText;
