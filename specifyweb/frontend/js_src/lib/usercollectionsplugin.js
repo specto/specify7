2d707779ea85c0fe4d426cc52dbada55eb85a66a
Max Patiiuk <max@patii.uk>
2021-11-08 08:48:38 -0600

    Don't use input[type="button"]
    Use button[type="button"] instead

--- specifyweb/frontend/js_src/lib/usercollectionsplugin.js
+++ specifyweb/frontend/js_src/lib/usercollectionsplugin.js
@@ -65,7 +65,7 @@ module.exports =  UIPlugin.extend({
     },
     render: function() {
         Q.all([this.user.fetch(), this.allCollections.fetch({limit:0})]).then(() => {
-            this.$el.attr('value', adminText('collections'));
+            this.el.innerText = adminText('collections');
             this.user.isNew() && this.$el.attr('title', adminText('saveUserFirst')).prop('disabled', true);
         });
         return this;
